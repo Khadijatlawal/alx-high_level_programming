1fc3cd73172905d3a306c63e823539a3d90d5318
KhadijatLawal <ddsglams@gmail.com>
2022-09-15 18:01:21 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,29 @@ void print_integer(int m);
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		print_integer(n * -1);
 	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	else
+		print_integer(n);
+}
+
+/**
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
 }
