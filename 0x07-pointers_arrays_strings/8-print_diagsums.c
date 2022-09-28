7bdaaffba2c2128e7f96bb39b387cdc4783bda12
KhadijatLawal <ddsglams@gmail.com>
2022-09-27 19:32:58 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,13 @@ nclude <stdio.h>
  */
 void print_diagsums(int *a, int size)
 {
-	int i, n, total1 = 0, total2 = 0;
+	int i = 0, max = size * size, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		total1 = total1 + a[i];
+	for (; i < max; i += size + 1)
+		sum1 += a[i];
 
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-		total2 = total2 + a[n];
-	printf("%d, %d\n", total1, total2);
+	for (i = size - 1; i < max - 1; i += size - 1)
+				sum2 += a[i];
+
+	printf("%d, %d\n", sum1, sum2);
 }
