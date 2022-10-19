4fed76667fe83732d1fb5e35438fbc6482162c11
KhadijatLawal <ddsglams@gmail.com>
2022-10-19 00:33:02 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,7 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 /**
- * main - Entry Point
- * Return: 0
+ * main - prints the name of file
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
